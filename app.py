5f5775b2ae7d357f6a8393ab985f501b109e1c4b
einstein2306 <pasinyaswanth2004@gmail.com>
2025-03-07 21:33:01 +0530

    added more arithematic calculators

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def game():
     return render_template("game.html")
+
+@app.route("/subtraction")
+def subtraction():
+    return render_template("subtraction.html")
+
+@app.route("/multiplication")
+def multiplication():
+    return render_template("multiplication.html")
+    
